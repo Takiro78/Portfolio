14cddeaf440064712536f094ca9966d44f757a5d
Gabriel Mathura <gabriel.mathura@gmail.com>
2026-03-07 21:46:15 +0000

    made wave only appear on home page

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -19,7 +19,6 @@ export default function App() {
   return (
     <div className="container">
       <Nav />
-      <Wave />
       
 
 
--- my-react-app/src/pages/home/Home.jsx
+++ my-react-app/src/pages/home/Home.jsx
@@ -1,23 +1,25 @@
+import Wave from "../../Components/wave/Wave";
 
 
 export default function Home(){
     return (
+      <>
+        <Wave />
 
-              <div className="page-contents">
+
+        <div className="page-contents">
         
-        <div className="hero section">
+          <div className="hero section">
 
-            <h1>Welcom To Gabriel's Fabulous Portfolio</h1>
-            <div className="banner" >
-              <img className = "banner-img" src="Cayman4.png" style={{ width: "100%", height: "auto" }} />
-            </div>
-        </div>
+              <h1>Welcom To Gabriel's Fabulous Portfolio</h1>
+              <div className="banner" >
+                <img className = "banner-img" src="Cayman4.png" style={{ width: "100%", height: "auto" }} />
+              </div>
+          </div>
         
         {/* <div className="spacer"></div> */}
+        </div>
 
-
-       
-
-      </div>
+      </>
     )
 }
