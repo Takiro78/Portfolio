96fc3781edbe388148b2287a4786e2acb296dfcb
Gabriel Mathura <gabriel.mathura@gmail.com>
2026-03-07 22:03:50 +0000

    created file for projects page and did a little bit of work to it

---
+++ my-react-app/src/pages/projects/Projects.jsx
@@ -0,0 +1,21 @@
+
+
+export default function Projects() {
+
+    return (
+        <>
+            <h1>Projects</h1>
+
+            <div className={"projects-section"}>
+
+                <div className={"project-card"}>
+                    <h2>Title</h2>
+                    <img src="no_img.jpeg" alt="Projects" />
+                    <div className={"card-desc"}>
+                        <p>lorem ipsum</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
