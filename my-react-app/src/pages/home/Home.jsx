c5681fc5e4e7d9278e34ec71e957ca6b3f702da0
Gabriel Mathura <gabriel.mathura@gmail.com>
2026-03-09 17:40:18 +0000

    added some styling to about-me section

--- my-react-app/src/pages/home/Home.jsx
+++ my-react-app/src/pages/home/Home.jsx
@@ -28,15 +28,19 @@ export default function Home(){
                   <motion.div
                       initial={{ opacity:0}}
                       animate={{ opacity: 1 }}
-                      transition={{ duration:3, delay:4}}
+                      transition={{ duration:1.5, delay:4}}
                   >
                       <div className={"about-me section"}>
-                          <img src={"no_img.jpeg"} alt={"idk"}/>
-                          <div className={"about-me-text"}>
-                              <h2>Hello, I'm Gabriel Mathura</h2>
-                              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                          <div className={"about-me widget"}>
+                              <img src={"no_img.jpeg"} alt={"idk"}/>
+                              <div className={"about-me-text"}>
+                                  <h2>Hello, I'm Gabriel Mathura</h2>
+                                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                              </div>
                           </div>
                       </div>
+
+
                   </motion.div>
               </div>
 
