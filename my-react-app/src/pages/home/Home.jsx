dd5f2a89d4ebfcf2c56921eaa195c1cf838b8829
Gabriel Mathura <gabriel.mathura@gmail.com>
2026-03-09 13:18:20 +0000

    made what i want waves to look/behave like after they have settled without veritcal movement

--- my-react-app/src/pages/home/Home.jsx
+++ my-react-app/src/pages/home/Home.jsx
@@ -4,21 +4,29 @@ import Wave from "../../Components/wave/Wave";
 export default function Home(){
     return (
       <>
-        <Wave />
 
 
-        <div className="page-contents">
-        
-          <div className="hero section">
-
-              <h1>Welcom To Gabriel's Fabulous Portfolio</h1>
-              <div className="banner" >
-                <img className = "banner-img" src="Cayman4.png" style={{ width: "100%", height: "auto" }} />
+          <div className="page-contents">
+              <div className="hero section">
+                  <h1>Welcom To Gabriel's Fabulous Portfolio</h1>
+                  <div className="banner" >
+                      <img className = "banner-img" src="Cayman4.png" style={{ width: "100%", height: "auto" }} />
+                  </div>
               </div>
+
+              <Wave />
+
+              <div className={"about-me section"}>
+                  <img src={"no_img.jpeg"} alt={"idk"}/>
+                  <div className={"about-me-text"}>
+                      <h2>Hello, I'm Gabriel Mathura</h2>
+                      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                  </div>
+              </div>
+
           </div>
-        
-        {/* <div className="spacer"></div> */}
-        </div>
+
+
 
       </>
     )
