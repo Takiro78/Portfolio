dc70287bc786989417b66b910a1fa3a1182b7a5d
Gabriel Mathura <gabriel.mathura@gmail.com>
2026-03-09 13:49:48 +0000

    tried using original code to move wave

--- my-react-app/src/Components/wave/Wave.jsx
+++ my-react-app/src/Components/wave/Wave.jsx
@@ -10,127 +10,115 @@ export default function Wave() {
         <div className="wave-wrapper">
             
             {/**BACK WAVE vertical movement */}
-            {/*<motion.div*/}
-            {/*    className="wave-container"*/}
-            {/*    initial = {{y:-100}}*/}
-            {/*    animate = {{y:600}}*/}
-            {/*    transition={{ duration:4, ease:easeInOut}}*/}
-            {/*>*/}
+            <motion.div
+                className="wave-container"
+                initial = {{y:-750}}
+                animate = {{y:0}}
+                transition={{ duration:4, ease:easeInOut}}
+            >
 
                 {/* *horizontal movement */}
-            <div className="wave back">
-                <motion.div 
+                <div className="wave back">
+                    <motion.div
 
-                    animate = {{x: ["0%", "-100%"]}}
-                    transition={{duration: 10 , ease: "linear",repeat:Infinity}}
+                        animate = {{x: ["0%", "-100%"]}}
+                        transition={{duration: 10 , ease: "linear",repeat:Infinity}}
 
-                >
-                    <svg className="wave-svg" viewBox="0 0 2400 150" preserveAspectRatio="xMidYMid slice">
-                    <path
-                        className="wave-path"
-                        d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
-                        fill="#4fc3f7"
-                    />
-                    
-                    {/* second identical wave shifted right */}
-                    <path
-                        className="wave-path"
+                    >
+                        <svg className="wave-svg" viewBox="0 0 2400 150" preserveAspectRatio="xMidYMid slice">
+                        <path
+                            className="wave-path"
+                            d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
+                            fill="#4fc3f7"
+                        />
 
-                        d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
-                        fill="#4fc3f7"
-                        transform="translate(1200, 0)"
-                    />
-                    </svg>
-                </motion.div>
-            </div>
-            {/*</motion.div>*/}
+                        {/* second identical wave shifted right */}
+                        <path
+                            className="wave-path"
+
+                            d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
+                            fill="#4fc3f7"
+                            transform="translate(1200, 0)"
+                        />
+                        </svg>
+                    </motion.div>
+                </div>
+            </motion.div>
 
 
 
                 {/**middle WAVE vertical movement */}
-            {/*<motion.div*/}
-            {/*    className="wave-container"*/}
-            {/*    initial = {{y:-100}}*/}
-            {/*    animate = {{y:630}}*/}
-            {/*    transition={{ duration:4, ease:easeInOut}}*/}
-            {/*>*/}
+            <motion.div
+                className="wave-container"
+                initial = {{y:-750}}
+                animate = {{y:0}}
+                transition={{ duration:4, ease:easeInOut}}
+            >
 
                 {/* *horizontal movement */}
-            <div className="wave mid">
+                <div className="wave mid">
 
-                <motion.div 
-                    animate = {{x: ["-100%", "00%"]}}
-                    transition={{duration: 10 , ease: "linear",repeat:Infinity}}
+                    <motion.div
+                        animate = {{x: ["-100%", "00%"]}}
+                        transition={{duration: 10 , ease: "linear",repeat:Infinity}}
 
-                >
-                    <svg className="wave-svg" viewBox="0 0 2400 150" preserveAspectRatio="xMidYMid slice">
-                    <path
-                        d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
-                        fill="#2e7bce"
-                    />
-                    
-                    {/* second identical wave shifted right */}
-                    <path
-                        d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
-                        fill="#2e7bce"
-                        transform="translate(1200, 0)"
-                    />
-                    </svg>
-                </motion.div>
-            </div>
-            {/*</motion.div>*/}
+                    >
+                        <svg className="wave-svg" viewBox="0 0 2400 150" preserveAspectRatio="xMidYMid slice">
+                        <path
+                            d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
+                            fill="#2e7bce"
+                        />
+
+                        {/* second identical wave shifted right */}
+                        <path
+                            d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
+                            fill="#2e7bce"
+                            transform="translate(1200, 0)"
+                        />
+                        </svg>
+                    </motion.div>
+                </div>
+            </motion.div>
 
 
 
                 {/**BACK WAVE vertical movement */}
-            {/*<motion.div*/}
-            {/*    className="wave-container"*/}
-            {/*    initial = {{y:-100}}*/}
-            {/*    animate = {{y:660}}*/}
-            {/*    transition={{ duration:4, ease:easeInOut}}*/}
-            {/*>*/}
+            <motion.div
+                className="wave-container"
+                initial = {{y:-750}}
+                animate = {{y:0}}
+                transition={{ duration:4, ease:easeInOut}}
+            >
 
                 {/* *horizontal movement */}
                 <div className="wave front">
 
-                <motion.div 
-                    animate = {{x: ["0%", "-100%"]}}
-                    transition={{duration: 18 , ease: "linear",repeat:Infinity}}
+                    <motion.div
+                        animate = {{x: ["0%", "-100%"]}}
+                        transition={{duration: 18 , ease: "linear",repeat:Infinity}}
 
-                >
-                    <svg className="wave-svg" viewBox="0 0 2400 150" preserveAspectRatio="xMidYMid slice">
-                    <path
-                        d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
-                        fill="#0848b7"
+                    >
+                        <svg className="wave-svg" viewBox="0 0 2400 150" preserveAspectRatio="xMidYMid slice">
+                            <path
+                                d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
+                                fill="#0848b7"
 
-                    />
-                    
-                    {/* second identical wave shifted right */}
-                    <path
-                        d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
-                        fill="#0848b7"
-                        transform="translate(1200, 0)"
-                    />
-                    </svg>
-                {/*</motion.div>*/}
+                            />
 
-                
-
-    
-
-
-            </motion.div>
+                            {/* second identical wave shifted right */}
+                            <path
+                                d="M0 80 C 100 40, 200 120, 300 80 C 400 40, 500 120, 600 80 C 700 40, 800 120, 900 80 C 1000 40, 1100 120, 1200 80 L 1200 150 L 0 150 Z"
+                                fill="#0848b7"
+                                transform="translate(1200, 0)"
+                            />
+                        </svg>
+                    </motion.div>
                 </div>
 
-            {/*<motion.div*/}
-            {/*    className="wave-bg"*/}
-            {/*    initial={{ y: 0, height: "100vh" }}*/}
-            {/*    animate={{ y: 800, height: "15vh" }}*/}
-            {/*    transition={{ duration: 4, ease: easeInOut }}*/}
-            {/*    />*/}
+            </motion.div>
 
 
-               
+
 
         </div>
     )    
